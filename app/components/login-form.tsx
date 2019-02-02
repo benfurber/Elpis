8f9f033c9688504ee340e14eb4b370a1b6149ff4
Ben Furber <ben@fu.rber.io>
2019-02-02 18:47:23 +0000

    🎨 Add styling to LoginForm

--- app/components/login-form.tsx
+++ app/components/login-form.tsx
@@ -1,13 +1,64 @@
 import React from "react";
-import { View, Text } from "react-native";
+import {
+  StyleSheet,
+  TextInput,
+  View,
+  Text,
+  TouchableOpacity
+} from "react-native";
 
 const LoginForm = () => {
   return (
-    <View>
-      <Text>Email</Text>
-      <Text>Password</Text>
+    <View style={styles.container}>
+      <View style={styles.row}>
+        <TextInput style={styles.input} value="Email" />
+      </View>
+      <View style={styles.row}>
+        <TextInput style={styles.input} value="Password" />
+      </View>
+      <View style={styles.row}>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: "rgba(4, 71, 47, 0.75)",
+    borderRadius: 100,
+    color: "rgb(255, 255, 255)",
+    paddingHorizontal: 20,
+    paddingVertical: 10
+  },
+  buttonText: {
+    color: "rgb(255, 255, 255)",
+    fontSize: 16,
+    fontWeight: "bold"
+  },
+  container: {
+    alignItems: "center",
+    flexDirection: "column",
+    flex: 1,
+    justifyContent: "center"
+  },
+  label: {
+    flex: 1
+  },
+  input: {
+    backgroundColor: "rgb(255, 255, 255)",
+    borderRadius: 5,
+    flex: 2,
+    height: 40,
+    padding: 10
+  },
+  row: {
+    alignItems: "center",
+    flexDirection: "row",
+    margin: 10
+  }
+});
 
 export { LoginForm };
