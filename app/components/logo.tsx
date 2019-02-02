ca064e2df5709757bceeb45dc6ee428e48a05c1e
Ben Furber <ben@fu.rber.io>
2019-02-02 18:46:46 +0000

    🎨 Add basic styling to Logo

--- app/components/logo.tsx
+++ app/components/logo.tsx
@@ -3,17 +3,34 @@ import { View, StyleSheet, Text } from "react-native";
 
 const Logo = () => {
   return (
-    <View>
-      <Text style={styles.logo}>Elpis</Text>
+    <View style={styles.container}>
+      <View style={styles.textBox}>
+        <Text style={styles.text}>Elpis</Text>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  logo: {
-    fontSize: 20,
-    textAlign: "center",
-    margin: 10
+  container: {
+    alignItems: "flex-end",
+    flex: 1,
+    flexDirection: "column"
+  },
+  text: {
+    alignSelf: "flex-end",
+    color: "#333",
+    flex: 1,
+    fontSize: 40,
+    margin: 10,
+    textAlign: "center"
+  },
+  textBox: {
+    alignItems: "center",
+    alignSelf: "flex-end",
+    flex: 1,
+    flexDirection: "row",
+    width: "100%"
   }
 });
 
