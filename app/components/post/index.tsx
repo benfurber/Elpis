b5563971e57b394e1dfb6ab7df366a51d374efda
Ben Furber <ben@fu.rber.io>
2019-02-27 18:13:42 +0000

    ♻️ Refactor footer for Post into sub-component

--- app/components/post/index.tsx
+++ app/components/post/index.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import FlexImage from "react-native-flex-image";
 
 import { colours, layout } from "styles";
-import { Icon } from "components";
 
+import { Footer } from "./footer";
 import { Tabs } from "./tabs";
 
 interface Props {
@@ -23,41 +23,12 @@ const Post = (props: Props) => {
         />
         <Text style={styles.description}>{description}</Text>
       </View>
-      <View style={styles.footer}>
-        <View style={styles.followingContainer}>
-          <TouchableOpacity style={styles.followingButton}>
-            <Text style={styles.followingButtonText}>Following</Text>
-            <Icon name="user-check" />
-          </TouchableOpacity>
-        </View>
-        <View style={styles.avatarContainer}>
-          <Image
-            source={require("../../assets/images/empower_two_women_logo.png")}
-            style={styles.authorAvatar}
-          />
-        </View>
-      </View>
+      <Footer />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  authorAvatar: {
-    backgroundColor: colours.navyBlueDark,
-    borderColor: colours.navyBlueDark,
-    borderRadius: 30,
-    borderWidth: 5,
-    height: 60,
-    overflow: "hidden",
-    position: "absolute",
-    top: -60,
-    width: 60
-  },
-  avatarContainer: {
-    alignItems: "center",
-    flex: 1,
-    flexDirection: "column"
-  },
   body: {
     backgroundColor: colours.whiteTransparent,
     borderTopRightRadius: layout.borderRadius,
@@ -73,30 +44,6 @@ const styles = StyleSheet.create({
   image: {
     borderRadius: layout.borderRadius,
     overflow: "hidden"
-  },
-  followingButton: {
-    alignSelf: "flex-end",
-    alignItems: "center",
-    backgroundColor: colours.whiteTransparent,
-    borderRadius: 99,
-    flex: 1,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    paddingVertical: 5,
-    paddingHorizontal: 10
-  },
-  followingContainer: {
-    backgroundColor: colours.navyBlueDark,
-    borderBottomLeftRadius: layout.borderRadius,
-    borderBottomRightRadius: layout.borderRadius,
-    flex: 1,
-    padding: layout.padding
-  },
-  followingButtonText: {
-    paddingRight: 10
-  },
-  footer: {
-    backgroundColor: colours.whiteTransparent
   }
 });
 
