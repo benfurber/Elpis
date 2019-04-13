87c445d4a1fd181f637b3bbec37777da3b3d12f7
Ben Furber <ben@fu.rber.io>
2019-04-13 12:43:59 +0100

    Add replies data

--- app/screens/feed.tsx
+++ app/screens/feed.tsx
@@ -9,6 +9,27 @@ interface Props {
   navigation: NavigationType;
 }
 
+const reply1 = {
+  author: {
+    avatarPath: require("assets/images/profile-pic-annon.png"),
+    name: "Kelli S"
+  },
+  body:
+    "Meu pai também fez o mesmo. Logo depois que minha mãe pediu o divórcio porque sofria violência dentro de casa, ele pegou as malas, se mudou e sumiu no mundo. Nem faço mais questão de saber pra onde ele foi.",
+  dateCreated: new Date("2019-01-02"),
+  id: "21098"
+};
+
+const reply2 = {
+  author: {
+    avatarPath: require("assets/images/profile-pic-annon.png"),
+    name: "Kelli S"
+  },
+  body: "Nem faço mais questão de saber pra onde ele foi.",
+  dateCreated: new Date("2019-01-03"),
+  id: "21099"
+};
+
 const comment = {
   author: {
     avatarPath: require("assets/images/profile-pic-may.jpg"),
@@ -18,8 +39,9 @@ const comment = {
     "Dá certo sim, o meu pai, por exemplo, fugiu quando eu tinha 5 anos e eu não faço ideia da onde ele esteja. Nunca mais voltou, pena que a violência só piorou.",
   dateCreated: new Date("2019-01-01"),
   id: "21097",
+  replies: [reply1, reply2],
   title: "Meu pai fez o que ela mandou…",
-  totalReplies: 4
+  totalReplies: 2
 };
 
 const posts = [
