97f041f4e07f52f3292eeacf8a25496288c2925b
Ben Furber <ben@fu.rber.io>
2019-05-17 16:06:19 +0100

    ✨ 📸 Add snapshot test to cover scenario

--- app/components/comments/__tests__/header.test.tsx
+++ app/components/comments/__tests__/header.test.tsx
@@ -53,4 +53,39 @@ describe("Comments<Header>", () => {
       expect(component).toMatchSnapshot();
     });
   });
+
+  describe("without description", () => {
+    it("renders correctly", () => {
+      const reply = {
+        author: {
+          avatarPath: require("assets/images/profile-pic-may.jpg"),
+          name: "Benita F"
+        },
+        body: "Nao.",
+        dateCreated: new Date("2019-01-01"),
+        id: "21097"
+      };
+      const comment = {
+        author: {
+          avatarPath: require("assets/images/profile-pic-may.jpg"),
+          name: "May F"
+        },
+        body:
+          "Dá certo sim, o meu pai, por exemplo, fugiu quando eu tinha 5 anos e eu não faço ideia da onde ele esteja. Nunca mais voltou, pena que a violência só piorou.",
+        dateCreated: new Date("2019-01-01"),
+        id: "21097",
+        title: "Meu pai fez o que ela mandou…",
+        totalReplies: 1,
+        replies: [reply]
+      };
+
+      const comments = [comment];
+
+      const component = shallow(
+        <Header comments={comments} description={null} />
+      );
+
+      expect(component).toMatchSnapshot();
+    });
+  });
 });
