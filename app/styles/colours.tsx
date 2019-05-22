72bf54e73d014ea7bae909a694036e285db6d983
Ben Furber <ben@fu.rber.io>
2019-05-22 20:27:18 +0100

    ✨ 🎨 Add error display for LoginForm

--- app/styles/colours.tsx
+++ app/styles/colours.tsx
@@ -5,6 +5,8 @@ const colours = {
   navyBlueDark: "rgb(45, 82, 113)",
   navyBlueDarkTransparentHigh: "rgba(45, 82, 113, 0.60)",
   pureWhite: "rgb(255, 255, 255)",
+  red: "rgb(208, 2, 27)",
+  redTransparent: "rgba(208, 2, 27, 0.75)",
   transparentBlue: "rgba(198, 213, 225, 0.75)",
   whiteTransparent: "rgba(255, 255, 255, 0.85)",
   whiteTransparentHigh: "rgba(255, 255, 255, 0.60)",
