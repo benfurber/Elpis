478fbd4b8634c266a868e6f82c9e79be7e99a163
Ben Furber <ben@fu.rber.io>
2019-06-09 15:33:25 +0100

    ✨ Add MessageBox for form messages

--- app/components/index.tsx
+++ app/components/index.tsx
@@ -6,6 +6,7 @@ export { FormCompleteProfile } from "./form-complete-profile";
 export { Icon } from "./icon";
 export { LoginForm } from "./login-form";
 export { Logo } from "./logo";
+export { MessageBox } from "./message-box";
 export { Post } from "./post";
 export { ProfilePictureField } from "./profile-picture-field";
 export { TextField } from "./text-field";
