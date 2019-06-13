b81528f2f2892fd6375fdab49f25cdc27cb45652
Ben Furber <ben@fu.rber.io>
2019-06-13 08:58:23 +0100

    ✨ Add basic client

--- app/utils/index.tsx
+++ app/utils/index.tsx
@@ -1,6 +1,7 @@
 export { Analytics } from "./analytics";
 export { calculateTotalComments } from "./calculate-total-comments";
 export { checkPasswordStrength } from "./check-password-strength";
+export { client } from "./client";
 export { firstSentence } from "./first-sentence";
 export { formatDate } from "./format-date";
 export { sendImage } from "./send-image";
