b8a543e4e0086d3a48f6d0c4c3b9d06193414f05
Ben Furber <ben@fu.rber.io>
2019-06-13 08:58:23 +0100

    ✨ Add basic client

--- app/utils/index.tsx
+++ app/utils/index.tsx
@@ -1,4 +1,5 @@
 export { checkPasswordStrength } from "./check-password-strength";
+export { client } from "./client";
 export { firstSentence } from "./first-sentence";
 export { formatDate } from "./format-date";
 export { totalComments } from "./total-comments";
