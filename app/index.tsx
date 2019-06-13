91f66dae480b7a79ecb6b290dbcbb980804c00f2
Ben Furber <ben@fu.rber.io>
2019-06-13 08:58:42 +0100

    ✨ Add client to AppContainer

--- app/index.tsx
+++ app/index.tsx
@@ -2,16 +2,22 @@ import React from "react";
 import { AppRegistry } from "react-native";
 import { createAppContainer } from "react-navigation";
 import { setCustomText } from "react-native-global-props";
+import { ApolloProvider } from "react-apollo";
 
 import { RootStack } from "routes";
 import { elements } from "styles";
+import { client } from "utils";
 
 const AppContainer = createAppContainer(RootStack);
 
 setCustomText({ style: { ...elements.standardText } });
 
 const App = () => {
-  return <AppContainer />;
+  return (
+    <ApolloProvider client={client}>
+      <AppContainer />
+    </ApolloProvider>
+  );
 };
 
 export default App;
