280ae97a10cea20dcdff1afe58ad836b629e6fa7
Ben Furber <ben@fu.rber.io>
2019-06-14 09:11:09 +0100

    ✨ Add Config util

--- app/utils/index.tsx
+++ app/utils/index.tsx
@@ -1,5 +1,9 @@
+import Config from "./config";
+
 export { checkPasswordStrength } from "./check-password-strength";
 export { client } from "./client";
 export { firstSentence } from "./first-sentence";
 export { formatDate } from "./format-date";
 export { totalComments } from "./total-comments";
+
+export { Config };
