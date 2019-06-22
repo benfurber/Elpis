228c03d0219a263ba9fc41306854d0bf9a802781
Ben Furber <ben@fu.rber.io>
2019-06-22 11:02:47 +0100

    ♻️ Replace `apollo-boost` with more mature setup

--- app/utils/client.tsx
+++ app/utils/client.tsx
@@ -1,11 +1,32 @@
-import ApolloClient from "apollo-boost";
+import fetch from "node-fetch";
+
+import { ApolloClient } from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { HttpLink } from "apollo-link-http";
+import { onError } from "apollo-link-error";
+import { ApolloLink } from "apollo-link";
 
 import { Config } from "utils";
 
 const uri = Config.API_PATH;
 
 const client = new ApolloClient({
-  uri,
+  link: ApolloLink.from([
+    onError(({ graphQLErrors, networkError }) => {
+      if (graphQLErrors)
+        graphQLErrors.map(({ message, locations, path }) =>
+          console.log(
+            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+          ),
+        );
+      if (networkError) console.log(`[Network error]: ${networkError}`);
+    }),
+    new HttpLink({
+      uri,
+      fetch: fetch,
+    }),
+  ]),
+  cache: new InMemoryCache(),
 });
 
 export { client };
