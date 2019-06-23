8043d19e99c98663d9e9dd4ee945a25a822250f5
Ben Furber <ben@fu.rber.io>
2019-06-23 08:50:59 +0100

    ♻️ Tidy-up client for readability

--- app/utils/client.tsx
+++ app/utils/client.tsx
@@ -1,32 +1,35 @@
-import fetch from "node-fetch";
-
 import { ApolloClient } from "apollo-client";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { HttpLink } from "apollo-link-http";
 import { onError } from "apollo-link-error";
 import { ApolloLink } from "apollo-link";
+import fetch from "node-fetch";
 
 import { Config } from "utils";
 
 const uri = Config.API_PATH;
 
+const error = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors)
+    graphQLErrors.map(({ message, locations, path }) =>
+      console.log(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+      ),
+    );
+  if (networkError) console.log(`[Network error]: ${networkError}`);
+});
+
+const htmlLink = new HttpLink({
+  uri,
+  fetch: fetch,
+});
+
+const link = ApolloLink.from([error, htmlLink]);
+const cache = new InMemoryCache();
+
 const client = new ApolloClient({
-  link: ApolloLink.from([
-    onError(({ graphQLErrors, networkError }) => {
-      if (graphQLErrors)
-        graphQLErrors.map(({ message, locations, path }) =>
-          console.log(
-            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
-          ),
-        );
-      if (networkError) console.log(`[Network error]: ${networkError}`);
-    }),
-    new HttpLink({
-      uri,
-      fetch: fetch,
-    }),
-  ]),
-  cache: new InMemoryCache(),
+  link,
+  cache,
 });
 
 export { client };
