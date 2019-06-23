66b4d7a52a63fbe51045b3da4047d11e1e7b7da8
Ben Furber <ben@fu.rber.io>
2019-06-23 09:49:27 +0100

    ✨ Add Image to NoContent component

--- app/components/comments/no-content.tsx
+++ app/components/comments/no-content.tsx
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
-import { StyleSheet, View } from "react-native";
+import { Image, StyleSheet, View } from "react-native";
 
 import { Title } from "components";
-import { layout } from "styles";
+import { layout, typography } from "styles";
+
+const noCommentsBoxPath = "../../assets/images/no-comments-box.png";
 
 interface Props {
   text: string;
@@ -12,7 +14,8 @@ class NoContent extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Title text={this.props.text} />
+        <Image style={styles.image} source={require(noCommentsBoxPath)} />
+        <Title style={styles.title} text={this.props.text} />
       </View>
     );
   }
@@ -21,7 +24,17 @@ class NoContent extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     paddingHorizontal: layout.spacing,
-    paddingVertical: layout.spacingL,
+    paddingTop: layout.spacingL,
+    paddingBottom: layout.spacingXL,
+  },
+  image: {
+    alignSelf: "center",
+  },
+  title: {
+    alignContent: "center",
+    alignSelf: "center",
+    fontSize: typography.fontSizeXL,
+    textAlign: "center",
   },
 });
 
