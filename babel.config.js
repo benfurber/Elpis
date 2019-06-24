366941dfa79dc0e1671f7a11f8da8f5fc68f39da
Ben Furber <ben@fu.rber.io>
2019-06-24 19:00:40 +0100

    ♻️ Refactor babel config into js file

---
+++ babel.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  plugins: [
+    [
+      "module-resolver",
+      {
+        alias: {
+          app: "./app",
+          assets: "./app/assets",
+          components: "./app/components",
+          interfaces: "./app/interfaces",
+          routes: "./app/routes",
+          screens: "./app/screens",
+          styles: "./app/styles",
+          utils: "./app/utils",
+        },
+      },
+    ],
+  ],
+  presets: ["module:metro-react-native-babel-preset"],
+};
