81a2dd6e93a3987b5af2124f9e26597974929e40
Ben Furber <ben@fu.rber.io>
2019-06-27 08:43:23 +0100

    🗑 Drop Config util

--- app/utils/index.tsx
+++ app/utils/index.tsx
@@ -1,8 +1,4 @@
-import Config from "./config";
-
 export { checkPasswordStrength } from "./check-password-strength";
 export { firstSentence } from "./first-sentence";
 export { formatDate } from "./format-date";
 export { totalComments } from "./total-comments";
-
-export { Config };
