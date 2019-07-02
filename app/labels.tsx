50d4ab418a9585294867e5e00e6483d663706da3
Ben Furber <ben@fu.rber.io>
2019-07-02 08:16:48 +0100

    ✨ Add all labels into single location

---
+++ app/labels.tsx
@@ -0,0 +1,40 @@
+const labels = {
+  addPhoto: "Add photo",
+  addYourComment: "Adicione o seu comentário...",
+  addYourReply: "Adicione o seu resposta...",
+  backButton: "Voltar aos Comentários",
+  comment: "Comentar",
+  comments: "Comentários",
+  email: "Email",
+  enter: "Enter",
+  formButton: "Submit",
+  formTitle: "Please complete your profile.",
+  imageEmpty: "No profile picture selected.",
+  login: "Entrar",
+  noComments: "Nenhum comentário ainda - faça o primeiro!",
+  noReplies: "Nenhuma resposta ainda - faça a primeira!",
+  password: "Senha",
+  passwordSet: "Set password",
+  passwordEmpty: "Password field empty.",
+  passwordMismatch: "The passwords don't match.",
+  passwordMedium: "Password nearly strong enough.",
+  passwordRepeat: "Repeat password",
+  passwordRequest: "A nice strong password please.",
+  passwordStrong: "Great password! Nice one.",
+  passwordWeak:
+    "Password too weak. Must include:\n" +
+    "- 1+ uppercase letter\n- 1+ lowercase letter\n- 1+ number",
+  reply: "Resposta",
+  replies: "respostas",
+  select: "Select",
+  thankYouText:
+    "Please remember to provide as much feedback as you can.\r\r" +
+    "We won’t be able to build something great unless you tell us what you think.",
+  thankYouTitle: "Please complete your profile",
+  topics: "Tópicos",
+  welcomeButton: "Next",
+  welcomeTitle: "Bem vinda à Elpis, ",
+  welcomeText: "Lots of words here. ",
+};
+
+export { labels };
