ca201c734ffbeff757ca76f1a332a6f50bfd6a0e
Ben Furber <ben@fu.rber.io>
2019-08-03 14:41:52 +0100

    🔧 Fix some query bug

--- app/queries.tsx
+++ app/queries.tsx
@@ -32,6 +32,7 @@ export const comments = gql`
       comments {
         id
         author {
+          id
           name
           avatarPath
         }
@@ -56,6 +57,7 @@ export const commentWithReplies = gql`
       replies {
         id
         author {
+          id
           name
           avatarPath
         }
