4d306d82129850ffd8bc4c6b0757b6ac74b5db0a
Ben Furber <ben@fu.rber.io>
2019-08-03 20:24:50 +0100

    ✨ Add extra test to for better coverage

--- app/utils/__tests__/drop-first-sentence.test.tsx
+++ app/utils/__tests__/drop-first-sentence.test.tsx
@@ -8,4 +8,12 @@ describe("dropFirstSentence", () => {
 
     expect(response).toEqual("Second sentence.");
   });
+
+  it("returns null if there's only a single short sentence", () => {
+    const multiSentenceString = "One sentence.";
+
+    const response = dropFirstSentence(multiSentenceString);
+
+    expect(response).toEqual(null);
+  });
 });
