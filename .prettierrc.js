48c84ec9a5b20b07c591078f8f4db66e786b75ff
Ben Furber <ben@fu.rber.io>
2019-09-22 16:40:21 +0100

    🗑 Drop brackets rule add from upgrade

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,9 +1,8 @@
 module.exports = {
-  bracketSpacing: false,
   jsxBracketSameLine: true,
   printWidth: 80,
   semi: true,
   singleQuote: false,
   tabWidth: 2,
-  trailingComma: "all"
+  trailingComma: "all",
 };
