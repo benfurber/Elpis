dfc29366704f09c87fe43daefb27bc0770178e14
Ben Furber <ben@fu.rber.io>
2019-10-07 19:11:34 +0100

    🔧 Fix circular variable declarations

--- app/utils/__tests__/calculate-total-comments.test.tsx
+++ app/utils/__tests__/calculate-total-comments.test.tsx
@@ -1,15 +1,27 @@
+import { Reply } from "interfaces";
 import { author } from "../../factories";
 
 import { calculateTotalComments } from "utils";
 
 describe("calculateTotalComments()", () => {
   it("returns the total number of comments", () => {
+    const comment = {
+      author,
+      content:
+        "Dá certo sim, o meu pai, por exemplo, fugiu quando eu tinha 5 anos e eu não faço ideia da onde ele esteja. Nunca mais voltou, pena que a violência só piorou.",
+      createdAt: new Date("2019-01-01"),
+      id: "21097",
+      replies: [] as Reply[],
+      totalReplies: 2,
+    };
+
     const reply1 = {
       author,
       content:
         "Meu pai também fez o mesmo. Logo depois que minha mãe pediu o divórcio porque sofria violência dentro de casa, ele pegou as malas, se mudou e sumiu no mundo. Nem faço mais questão de saber pra onde ele foi.",
       createdAt: new Date("2019-01-02"),
       id: "21098",
+      comment,
     };
 
     const reply2 = {
@@ -17,17 +29,11 @@ describe("calculateTotalComments()", () => {
       content: "Nem faço mais questão de saber pra onde ele foi.",
       createdAt: new Date("2019-01-03"),
       id: "21099",
+      comment,
     };
 
-    const comment = {
-      author,
-      content:
-        "Dá certo sim, o meu pai, por exemplo, fugiu quando eu tinha 5 anos e eu não faço ideia da onde ele esteja. Nunca mais voltou, pena que a violência só piorou.",
-      createdAt: new Date("2019-01-01"),
-      id: "21097",
-      replies: [reply1, reply2],
-      totalReplies: 2,
-    };
+    comment.replies.push(reply1 as Reply);
+    comment.replies.push(reply2 as Reply);
 
     const comments = [comment];
 
