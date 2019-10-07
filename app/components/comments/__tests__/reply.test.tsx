631a2cf9f76819e9754657b16ee3c599dbe2c4c3
Ben Furber <ben@fu.rber.io>
2019-10-07 19:10:51 +0100

    🔧 Fix typing on Comments.Reply test

--- app/components/comments/__tests__/reply.test.tsx
+++ app/components/comments/__tests__/reply.test.tsx
@@ -2,7 +2,7 @@ import "react-native";
 import React from "react";
 import { shallow } from "enzyme";
 
-import { author } from "../../../factories";
+import { author, comment } from "../../../factories";
 import { mockDateNow } from "../../../test-utils";
 
 import { Reply } from "../reply";
@@ -17,6 +17,7 @@ describe("Comments<Reply>", () => {
         "Meu pai também fez o mesmo. Logo depois que minha mãe pediu o divórcio porque sofria violência dentro de casa, ele pegou as malas, se mudou e sumiu no mundo. Nem faço mais questão de saber pra onde ele foi.",
       createdAt: new Date("2019-01-02"),
       id: "21098",
+      comment,
     };
 
     const component = shallow(<Reply item={item} />);
