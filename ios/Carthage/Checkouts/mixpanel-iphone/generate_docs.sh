00f2173330cce8baf42b0500d086fe4fabe03aea
Ben Furber <ben@fu.rber.io>
2019-10-27 16:04:31 +0000

    ✨ Add current ios package to carthage

---
+++ ios/Carthage/Checkouts/mixpanel-iphone/generate_docs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Generating documentation."
+
+rm -rf docs
+appledoc --project-name mixpanel-iphone --project-company Mixpanel --company-id com.mixpanel --no-repeat-first-par --keep-undocumented-objects --keep-undocumented-members --preprocess-headerdoc --no-create-docset --output docs Mixpanel/Mixpanel.h Mixpanel/MixpanelPeople.h Mixpanel/MixpanelGroup.h
+cp -a docs/html/. docs/.
+rm -rf docs/html
+
+echo "Updated docs!"
