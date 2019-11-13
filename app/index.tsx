dc3d446d078175f5829ef2c14b7fcd3addd18eb7
Ben Furber <ben@fu.rber.io>
2019-11-13 18:54:47 +0000

    ✨ Add basic setup to app

--- app/index.tsx
+++ app/index.tsx
@@ -2,8 +2,11 @@ import React, { Component } from "react";
 import { AppRegistry } from "react-native";
 import { createAppContainer } from "react-navigation";
 import { setCustomText } from "react-native-global-props";
+import OneSignal from "react-native-onesignal";
 import { ApolloProvider } from "@apollo/react-hooks";
 import RNBootSplash from "react-native-bootsplash";
+
+import { ONE_SIGNAL_ID } from "react-native-dotenv";
 
 import { RootStack } from "routes";
 import { elements } from "styles";
@@ -14,8 +17,38 @@ const AppContainer = createAppContainer(RootStack);
 setCustomText({ style: { ...elements.standardText } });
 
 class App extends Component {
+  constructor(properties) {
+    super(properties);
+    OneSignal.init(ONE_SIGNAL_ID);
+
+    OneSignal.addEventListener("received", this.onReceived);
+    OneSignal.addEventListener("opened", this.onOpened);
+    OneSignal.addEventListener("ids", this.onIds);
+  }
+
   componentDidMount() {
     RNBootSplash.hide({ duration: 250 });
+  }
+
+  componentWillUnmount() {
+    OneSignal.removeEventListener("received", this.onReceived);
+    OneSignal.removeEventListener("opened", this.onOpened);
+    OneSignal.removeEventListener("ids", this.onIds);
+  }
+
+  onReceived(notification) {
+    console.log("Notification received: ", notification);
+  }
+
+  onOpened(openResult) {
+    console.log("Message: ", openResult.notification.payload.body);
+    console.log("Data: ", openResult.notification.payload.additionalData);
+    console.log("isActive: ", openResult.notification.isAppInFocus);
+    console.log("openResult: ", openResult);
+  }
+
+  onIds(device) {
+    console.log("Device info: ", device);
   }
 
   render() {
