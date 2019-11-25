ff138465eda371db4c51901492311703b441128b
benfurber <ben@fu.rber.io>
2019-11-25 18:46:05 +0000

    Fix splash screen (#239)
    react-native-splash-screen wasn't working with RN 61 so it's been replaced with `react-native-bootsplash`. (Missing Android setup)
    
    * 🗑 Drop react-native-splash-screen
    * 📦 Add react-native-bootsplash package
    * ✨ Add launch screen to iOS

--- app/index.tsx
+++ app/index.tsx
@@ -3,12 +3,11 @@ import { AppRegistry } from "react-native";
 import { createAppContainer } from "react-navigation";
 import { setCustomText } from "react-native-global-props";
 import { ApolloProvider } from "@apollo/react-hooks";
+import RNBootSplash from "react-native-bootsplash";
 
 import { RootStack } from "routes";
 import { elements } from "styles";
 import { client } from "utils";
-
-import SplashScreen from "react-native-splash-screen";
 
 const AppContainer = createAppContainer(RootStack);
 
@@ -16,7 +15,7 @@ setCustomText({ style: { ...elements.standardText } });
 
 class App extends Component {
   componentDidMount() {
-    SplashScreen.hide();
+    RNBootSplash.hide({ duration: 250 });
   }
 
   render() {
