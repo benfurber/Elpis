1847576cefa26e67a6095a61f33395044857a62c
Zoe Kavanagh <zoe.kavanagh@concreteplatform.com>
2019-11-30 14:55:22 +0000

    Add sign out Icon to Setting

--- app/screens/settings.tsx
+++ app/screens/settings.tsx
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
-import { StyleSheet } from "react-native";
+import { StyleSheet, View, TouchableOpacity, Text } from "react-native";
 
-import { BackgroundContainer, Title } from "components";
+import { BackgroundContainer, Title, Icon } from "components";
 import { NavigationType } from "interfaces";
 import { labels } from "labels";
-import { layout, typography } from "styles";
+import { layout, typography, colours } from "styles";
 import { Analytics } from "utils";
 
 interface Props {
@@ -20,6 +20,17 @@ class SettingsScreen extends Component<Props> {
     return (
       <BackgroundContainer>
         <Title style={styles.title} text={labels.settings.title} />
+        <View>
+          <TouchableOpacity
+            style={styles.item}
+            onPress={() => null}
+          >
+            <View style={styles.row}>
+              <Icon name={"sign-out-alt"} />
+              <Text>Settings</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
       </BackgroundContainer>
     );
   }
@@ -29,6 +40,15 @@ const styles = StyleSheet.create({
   title: {
     fontSize: typography.fontSizeXL,
     margin: layout.spacingL,
+  },
+  item: {
+    backgroundColor: colours.whiteTransparent,
+    marginBottom: layout.spacing,
+    padding: layout.spacingL,
+  },
+  row: {
+    alignItems: "center",
+    flexDirection: "row",
   },
 });
 
