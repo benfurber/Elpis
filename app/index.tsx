91835a7a88281f555f319e71ff4673e3af6e1010
Ben Furber <ben@fu.rber.io>
2019-12-02 19:18:16 +0000

    🗑 Drop listeners

--- app/index.tsx
+++ app/index.tsx
@@ -22,35 +22,10 @@ class App extends Component {
   constructor(properties) {
     super(properties);
     OneSignal.init(ONE_SIGNAL_ID, { kOSSettingsKeyAutoPrompt: false });
-
-    OneSignal.addEventListener("received", this.onReceived);
-    OneSignal.addEventListener("opened", this.onOpened);
-    OneSignal.addEventListener("ids", this.onIds);
   }
 
   componentDidMount() {
     RNBootSplash.hide({ duration: 250 });
-  }
-
-  componentWillUnmount() {
-    OneSignal.removeEventListener("received", this.onReceived);
-    OneSignal.removeEventListener("opened", this.onOpened);
-    OneSignal.removeEventListener("ids", this.onIds);
-  }
-
-  onReceived(notification) {
-    console.log("Notification received: ", notification);
-  }
-
-  onOpened(openResult) {
-    console.log("Message: ", openResult.notification.payload.body);
-    console.log("Data: ", openResult.notification.payload.additionalData);
-    console.log("isActive: ", openResult.notification.isAppInFocus);
-    console.log("openResult: ", openResult);
-  }
-
-  onIds(device) {
-    console.log("Device info: ", device);
   }
 
   render() {
