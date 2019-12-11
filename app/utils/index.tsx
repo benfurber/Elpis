5352a2f4a9216d45deb4ae7844d79e25d14f8984
Ben Furber <ben@fu.rber.io>
2019-12-11 07:58:28 +0000

    🗑 Drop dropFirstSentence util

--- app/utils/index.tsx
+++ app/utils/index.tsx
@@ -2,7 +2,6 @@ export { Analytics, AnalyticsContentTypes } from "./analytics";
 export { calculateTotalComments } from "./calculate-total-comments";
 export { checkPasswordStrength } from "./check-password-strength";
 export { client } from "./client";
-export { dropFirstSentence } from "./drop-first-sentence";
 export { firstSentence } from "./first-sentence";
 export { formatDate } from "./format-date";
 export { pushNotifications } from "./push-notifications";
