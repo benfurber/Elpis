b515495a3bbeafe09349e3f329c2c553d3bbc139
Ben Furber <ben@fu.rber.io>
2019-12-12 19:45:33 +0000

    ✨ Add close button to WebBrowser

--- app/screens/web-browser.tsx
+++ app/screens/web-browser.tsx
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
-
+import { StyleSheet, TouchableOpacity, View } from "react-native";
 import { WebView } from "react-native-webview";
 import { withMappedNavigationParams } from "react-navigation-props-mapper";
 
-import { BackgroundContainer } from "components";
+import { BackgroundContainer, Icon } from "components";
 import { NavigationType } from "interfaces";
+import { layout, colours } from "styles";
 import { Analytics } from "utils";
 
 interface Props {
@@ -16,20 +17,54 @@ class WebBrowserScreen extends Component<Props> {
   componentDidMount() {
     Analytics.trackContent({
       contentType: "WebBrowser",
-      contentId: this.props.uri,
+      contentId: "",
     });
   }
 
   render() {
-    const { uri } = this.props;
+    const { navigation, uri } = this.props;
 
     return (
       <BackgroundContainer>
-        <WebView source={{ uri }} />
+        <View>
+          <View style={styles.closeContainer}>
+            <TouchableOpacity onPress={() => navigation.goBack()}>
+              <Icon name="times-circle" size={30} />
+            </TouchableOpacity>
+          </View>
+        </View>
+        <View>
+          <WebView
+            source={{ uri }}
+            containerStyle={{ flex: 0, height: "100%" }}
+          />
+        </View>
       </BackgroundContainer>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  body: {
+    backgroundColor: colours.whiteTransparent,
+    borderTopLeftRadius: layout.borderRadiusL,
+    borderTopRightRadius: layout.borderRadiusL,
+    flex: 1,
+    paddingTop: layout.spacing,
+  },
+  closeContainer: {
+    flexDirection: "row-reverse",
+    padding: layout.spacing,
+  },
+  ctaText: {
+    paddingHorizontal: layout.spacing,
+  },
+  row: {
+    alignItems: "baseline",
+    flexDirection: "row",
+  },
+});
+
 const wrappedWebBrowserScreen = withMappedNavigationParams()(WebBrowserScreen);
 export {
   wrappedWebBrowserScreen as WebBrowserScreen,
