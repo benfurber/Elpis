2fb66f9e2efd9e2e79d843973df5999d96b55cc4
Ben Furber <ben@fu.rber.io>
2019-12-23 11:42:11 +0000

    📦 Add Autolink to show URLs in rich text content

--- app/components/rich-text-display.tsx
+++ app/components/rich-text-display.tsx
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
+import Autolink from "react-native-autolink";
 
 import { LinkPreview } from "components";
 import { NavigationType, Reply as ReplyInterface } from "interfaces";
-import { layout } from "styles";
+import { colours, layout, typography } from "styles";
 
 interface Props {
   item: ReplyInterface;
@@ -11,14 +12,36 @@ interface Props {
 }
 
 class RichTextDisplay extends Component<Props> {
+  onPress() {
+    const { item, navigation } = this.props;
+    const { link } = item;
+
+    return navigation.navigate("Browser", { uri: link });
+  }
+
+  renderContent() {
+    const { content } = this.props.item;
+
+    if (!content) return null;
+
+    return (
+      <Autolink
+        onPress={() => this.onPress()}
+        text={content}
+        style={styles.body}
+        linkStyle={styles.link}
+      />
+    );
+  }
+
   render() {
     const { item, navigation } = this.props;
-    const { content, link } = item;
+    const { link } = item;
 
     return (
       <View>
         {link && <LinkPreview navigation={navigation} url={link} />}
-        <Text style={styles.body}>{content}</Text>
+        {this.renderContent()}
       </View>
     );
   }
@@ -26,7 +49,12 @@ class RichTextDisplay extends Component<Props> {
 
 const styles = StyleSheet.create({
   body: {
+    fontSize: typography.fontSize,
     paddingVertical: layout.spacing,
+  },
+  link: {
+    color: colours.darkGrey,
+    textDecorationLine: "underline",
   },
 });
 
