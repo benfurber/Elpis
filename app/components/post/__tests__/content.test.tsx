7f191047be80c66202ec37d2f30f928b16de00e4
benfurber <ben@fu.rber.io>
2020-01-07 18:28:21 +0000

    ✨ Add DisplayContent to render markdown (#283)
    Adds a new DisplayContent component to render markdown, added to display the content for posts.
    
    * 📦 Add react-native-markdown-display package
    * ✨ Add DisplayContent component to render markdown
    * ✨ Add DisplayContent to post content

--- app/components/post/__tests__/content.test.tsx
+++ app/components/post/__tests__/content.test.tsx
@@ -24,4 +24,29 @@ describe("Post<Content>", () => {
 
     expect(component).toMatchSnapshot();
   });
+
+  describe("with markdown", () => {
+    it("renders correctly", () => {
+      mockDateNow("2000-01-02");
+
+      const createdAt = new Date("2000-01-01");
+      const content = `# h1 Heading 8-\n
+      | Option | Description |\n
+      | ------ | ----------- |\n
+      | data   | path to data files to supply the data that will be passed into templates. |\n
+      | engine | engine to be used for processing templates. Handlebars is the default. |\n
+      | ext    | extension to be used for dest files. |\n`;
+      const imagePath = "assets/images/image_post_1.jpg";
+
+      const component = shallow(
+        <Content
+          createdAt={createdAt}
+          content={content}
+          imagePath={require(imagePath)}
+        />,
+      );
+
+      expect(component).toMatchSnapshot();
+    });
+  });
 });
