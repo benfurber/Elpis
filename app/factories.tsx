51f27c8908db5b0c284fdfc6c2fb971822c1cd18
Ben Furber <ben@fu.rber.io>
2020-01-12 20:34:43 +0000

    ♻️ Update factories to publishedAt

--- app/factories.tsx
+++ app/factories.tsx
@@ -1,43 +1,43 @@
 export const author = {
-  id: "1235we",
   avatarPath: require("assets/images/empower_two_women_logo.png"),
+  id: "1235we",
   name: "Empodere Duas Mulheres",
 };
 
 export const post = {
   author,
   comments: [],
-  createdAt: new Date("2000-01-01"),
   content: null,
   id: "10387-314fs-12asdbj",
   imagePath: require("assets/images/image_post_1.jpg"),
+  publishedAt: new Date("2000-01-01"),
 };
 
 export const preComment = {
   author,
   content:
     "Dá certo sim, o meu pai, por exemplo, fugiu quando eu tinha 5 anos e eu não faço ideia da onde ele esteja. Nunca mais voltou, pena que a violência só piorou.",
-  createdAt: new Date("2019-01-01"),
   id: "21097",
-  totalReplies: 0,
+  publishedAt: new Date("2019-01-01"),
   replies: [],
+  totalReplies: 0,
 };
 
 export const reply = {
   author,
-  content: "Nao.",
-  createdAt: new Date("2019-01-01"),
-  id: "21097",
   comment: preComment,
+  content: "Nao.",
+  id: "21097",
+  publishedAt: new Date("2019-01-01"),
 };
 
 export const comment = {
   author,
   content:
     "Dá certo sim, o meu pai, por exemplo, fugiu quando eu tinha 5 anos e eu não faço ideia da onde ele esteja. Nunca mais voltou, pena que a violência só piorou.",
-  createdAt: new Date("2019-01-01"),
   id: "21097",
-  totalReplies: 1,
+  publishedAt: new Date("2019-01-01"),
   replies: [reply],
   title: "Meu pai fez o que ela mandou",
+  totalReplies: 1,
 };
