93cc665974f6ea58f1ab16d32ce89e7e5f28bfbe
Ben Furber <ben@fu.rber.io>
2020-02-16 15:40:35 +0000

    ✨ Add author name to post footer

--- app/components/post/footer.tsx
+++ app/components/post/footer.tsx
@@ -1,21 +1,25 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 import { Avatar } from "components";
 import { Author } from "interfaces";
 import { colours, layout } from "styles";
 
 interface Props {
-  avatarPath: Author["avatarPath"];
+  author: Author;
 }
 
 const Footer = (props: Props) => {
+  const { avatarPath, name } = props.author;
+
   return (
     <View>
-      <View style={styles.followingContainer} />
+      <View style={styles.followingContainer}>
+        <Text style={styles.text}>{name}</Text>
+      </View>
       <View style={styles.avatarContainer}>
         <Avatar
-          avatarPath={props.avatarPath}
+          avatarPath={avatarPath}
           size={"large"}
           styles={styles.authorAvatar}
         />
@@ -32,16 +36,21 @@ const styles = StyleSheet.create({
     top: -50,
   },
   avatarContainer: {
-    alignItems: "center",
+    alignItems: "flex-end",
     flex: 1,
     flexDirection: "column",
+    marginRight: layout.spacing,
   },
   followingContainer: {
     backgroundColor: colours.navyBlueDark,
     borderBottomLeftRadius: layout.borderRadius,
     borderBottomRightRadius: layout.borderRadius,
     flex: 1,
-    padding: layout.spacingL,
+  },
+  text: {
+    color: colours.pureWhite,
+    fontWeight: "bold",
+    padding: layout.spacing,
   },
 });
 
