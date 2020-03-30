e28471334b8e03785127b66a025e9255660f01a5
Ben Furber <ben@fu.rber.io>
2020-03-30 10:45:27 +0100

    ⚙️ Add basic android fastlane setup

--- android/app/src/main/java/com/elpis/MainActivity.java
+++ android/app/src/main/java/com/elpis/MainActivity.java
@@ -1,4 +1,4 @@
-package com.elpis;
+package com.elpis.app;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
--- android/app/src/main/java/com/elpis/MainApplication.java
+++ android/app/src/main/java/com/elpis/MainApplication.java
@@ -1,4 +1,4 @@
-package com.elpis;
+package com.elpis.app;
 
 import android.app.Application;
 import android.content.Context;
